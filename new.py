50c77de3167b9a6c16ccdb6c352be9a449e2fe6c
Nur-2023 <nabusamra34@gmail.com>
2023-08-26 21:34:12 +0200

    first commit

---
+++ new.py
@@ -0,0 +1,71 @@
+# for i in range(1,6):
+#   for j in range(1,i+1):
+
+
+#*****************************************************************
+        #السؤال الثاني
+# def calculate_sum(number):
+#             sum_of_numbers = 0
+#
+#             for i in range(1, number + 1):
+#                 sum_of_numbers += i
+#
+#             return sum_of_numbers
+#
+#
+#         # Accept input from the user
+# user_number = int(input("Enter a number: "))
+#
+#
+#         # Call the function and store the result
+# result = calculate_sum(user_number)
+#
+# print("The sum of numbers from 1 to", user_number, "is", result)
+#*******************************************************************************
+# السؤال الثالث
+# def write_name(name) :
+#     return name
+# user_name=input('entre your name')
+# for index in user_name :
+#    for j in user_name :
+#     user_name+=index
+#     your_name= write_name(user_name)
+#     print(your_name)
+#***************************************************************
+#السؤال الرابع
+
+# def reverse_word(name) :
+#     return name
+# user_name=input('entre name : ')
+# result=user_name[::-1]
+# output= reverse_word(result)
+# print(output)
+
+
+#***********************************************************************
+# السؤال الخامس :
+# def count_down():
+#     return number
+# number=int(input('entre number : '))
+# while number>0 :
+#       print(number)
+#       number-=1
+# print(count_down())
+#**************************************************************************
+#السؤال السادس :
+# def multiples() :
+#     return num
+# num=int(input('entre number'))
+# while num :
+#  print(num)
+#  num+=5
+#
+#  if num > 50 :
+#       break
+#       print(multiples())
+
+#************************************************************************************
+#السؤال السابع :
+
+
+
